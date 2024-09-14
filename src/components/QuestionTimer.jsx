951f6fec4e57f3b2d6d6428a960430385b499081
Domagoj Vlaho <domagoj.vlaho@gmail.com>
2024-09-14 16:16:41 +0200

    Modify summary page and remove console logs

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,7 +4,6 @@ export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log("setting timeout");
     const timer = setTimeout(() => {
       onTimeout();
     }, timeout);
@@ -15,7 +14,6 @@ export default function QuestionTimer({ timeout, onTimeout, mode }) {
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log("setting interval");
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
